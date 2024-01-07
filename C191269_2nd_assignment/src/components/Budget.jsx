75d433975255741aabcb97e4de122eac8b435ae6
Disha-69 <jannathossaindisha@gmail.com>
2024-01-07 20:03:25 +0600

    assignment-2

---
+++ C191269_2nd_assignment/src/components/Budget.jsx
@@ -0,0 +1,22 @@
+export default function Budget() {
+  return (
+    <div className="mx-auto max-w-sm px-5 py-8 text-center text-white">
+      <div>
+        <h2>Available Budget</h2>
+        <p className="mt-1 text-4xl font-medium">+ BDT 18,000.00</p>
+      </div>
+
+      <div className="mt-4 flex items-center justify-between bg-green-500 px-4 py-3 text-sm">
+        <p>Income</p>
+        <p>
+          + BDT <span id="total-income">35,000.00</span>
+        </p>
+      </div>
+
+      <div className="mt-2 flex items-center justify-between bg-red-500 px-4 py-3 text-sm">
+        <span>Expenses</span>
+        <span>- BDT 17,000.00</span>
+      </div>
+    </div>
+  );
+}
